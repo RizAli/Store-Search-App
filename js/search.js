4115e6b57146fc6cf8791ff184eee3c1d72ca74e
Rizwan Ali <rizcube@gmail.com>
2015-03-15 21:02:10 +0000

    search prompt comes up and user can exit by typing quit'

---
+++ js/search.js
@@ -0,0 +1,14 @@
+
+var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var search;
+
+function print(message) {
+  document.write( '<p>' + message + '</p>');
+}
+
+while (true) {
+  search = prompt("Search for a product in our store. Type 'list' to show all of the products and 'quit' to exit");
+  if ( search === 'quit') {
+    break;
+  }
+}
